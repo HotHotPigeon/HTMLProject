a8a59742d16c7a4eb2f0be1bcd693e20b8ba193c
HotHotPigeon <wojtekgryzel@gmail.com>
2022-11-14 17:44:59 +0100

    Styled Menu Item

--- src/components/Navigation/MenuItem.vue
+++ src/components/Navigation/MenuItem.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">import router from '@/router';
+<script setup lang="ts">
 const props = defineProps({
     route: {
         type: Object,
@@ -9,7 +9,7 @@ const props = defineProps({
 
 <template v-if="route.path">
     <RouterLink :to="route.path" exact-path custom v-slot="{ href, isActive }">
-        <li :class="{ active: isActive }">
+        <li :class="{ active: isActive }" class="menu-item">
             <a :href="href">
                 <slot>
                     {{ route.name }}
@@ -20,11 +20,41 @@ const props = defineProps({
 </template>
 
 <style scoped>
-    .active {
-        font-weight: 700;
-    }
+.menu-item {
+    max-width: 216px;
+    /* until menu group doesnt have width*/
+    height: 40px;
+    border-radius: 8px;
+    list-style: none;
+    margin-bottom: 8px;
+    display: flex;
+    align-items: center;
+    padding-left: 22px;
+    font-size: 14px;
+    line-height: 17px;
+    position: relative;
+    overflow: hidden;
+}
 
-    .active::after {
-        content: ' Active';
-    }
+.menu-item:hover {
+    background-color: #0C0C0C;
+}
+
+.menu-item a {
+    text-decoration: none;
+}
+
+.active {
+    font-weight: 700;
+}
+
+.active::before {
+    content: '';
+    background: linear-gradient(167.45deg, #4F482B 0.1%, #B4AA81 39.79%, #826C31 101.58%);
+    position: absolute;
+    width: 6px;
+    top: 0;
+    bottom: 0;
+    left: 0;
+}
 </style>
